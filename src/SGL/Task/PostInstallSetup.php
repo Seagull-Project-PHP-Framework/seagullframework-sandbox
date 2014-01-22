041a624f9d4f086c13d77b90ce860310e776cf3c
Demian Turner <demian@phpkitchen.com>
2014-01-22 17:27:17 +0000

    no message

--- src/SGL/Task/PostInstallSetup.php
+++ src/SGL/Task/PostInstallSetup.php
@@ -4,6 +4,6 @@ class SGL_Task_PostInstallSetup extends SGL_Task
 {
     function run($conf = array())
     {
-        file_put_contents("/tmp/FOO", "test");
+        file_put_contents("/tmp/FOO3", "test");
     }
 }
